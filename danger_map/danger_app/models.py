8188c5da432ecc7d702cbf9f0c7f7f352caae89d
JayChung0101 <chung5587@naver.com>
2024-01-13 16:11:51 +0900

    Add Post fields
    Add 8 fields to model 'Post'

--- danger_map/danger_app/models.py
+++ danger_map/danger_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+class Post(models.Model):
+    head = models.CharField(max_length=20)
+    
+    content = models.TextField(blank=True)
+
+    content_pic = models.ImageField(upload_to='post_pics', blank=True)
+
+    DEGREE_CHOICES = [
+        (1, '10%'),
+        (2, '20%'),
+        (3, '30%'),
+        (4, '40%'),
+        (5, '50%'),
+        (6, '60%'),
+        (7, '70%'),
+        (8, '80%'),
+        (9, '90%'),
+        (10, '100%'),
+    ]
+    degree = models.IntegerField(choices=DEGREE_CHOICES, default=None)
+    
+    hash = models.CharField(max_length=10, blank=True)
+
+    dt_created = models.DateTimeField(auto_now_add=True)
+
+    dt_updated = models.DateTimeField(auto_now=True)
+
+
+class Like(models.Model):
+    pass
+
+
+class Dislike(models.Model):
+    pass
