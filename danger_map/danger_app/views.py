e53d13fc67ab73460ecf80374bc409c4ed598899
JayChung0101 <chung5587@naver.com>
2024-01-13 16:34:07 +0900

    Create PostListAPIView

--- danger_map/danger_app/views.py
+++ danger_map/danger_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import get_object_or_404, ListAPIView
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+class PostListAPIView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
