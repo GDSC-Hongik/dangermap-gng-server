4800dd2089e47d13ee8eb8657cae74f420161342
JayChung0101 <chung5587@naver.com>
2024-01-19 01:06:15 +0900

    User, Post, LikeAndDislike 시리얼라이저 작성

--- danger_map/danger_app/serializers.py
+++ danger_map/danger_app/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
-from .models import Post
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ['id', 'head', 'content', 'content_pic', 'degree', 'hash']
-        
+# 해당 프로젝트엔 파이썬 객체 모델이 따로 존재하지 않지만
+# 유효성 검사를 위한 시리얼라이저를 추가
+
+# 시리얼라이저는 딕셔너리를 반환
+
+class UserSerializer(serializers.Serializer):
+    email = serializers.CharField()
+    nickname = serializers.CharField()
+    profile_pic = serializers.URLField()
+
+
+# storage등록 후 content_pic URLField로 바꿔야 함
+class PostSerializer(serializers.Serializer):
+    content = serializers.CharField()
+    content_pic = serializers.CharField()
+    danger_rate = serializers.IntegerField(max_value=100, min_value=0)
+    title = serializers.CharField()
+    date = serializers.DateTimeField()
+    user_nickname = serializers.CharField()
+    user_email = serializers.CharField()
+
+
+# like, dislike 문서 추가를 위한 임시 시리얼라이저
+class LikeAndDislikeSerializer(serializers.Serializer):
+    user_email = serializers.CharField()
