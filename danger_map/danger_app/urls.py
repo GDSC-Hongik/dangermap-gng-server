763d4e4f5e95b777bd65a9aac8f226e4f316e982
JayChung0101 <chung5587@naver.com>
2024-01-19 01:08:03 +0900

    Edit 24.01.19

--- danger_map/danger_app/urls.py
+++ danger_map/danger_app/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import PostListAPIView
+from .views import get_user_list, get_user, get_post_list, get_post, add_like, add_dislike
 
 urlpatterns = [
-    path('posts/', PostListAPIView.as_view()),
+    # user 컬렉션 기반
+    path('users', get_user_list),
+    path('users/<str:pk>', get_user), # user_email을 pk로 받음
+
+    # post 컬렉션 기반
+    path('posts', get_post_list),
+    path('posts/<str:pk>', get_post), # post_title을 pk로 받음
+
+    # 좋아요, 싫어요 개수 추가. 아무 데이터도 입력할 필요 없이 POST요청만 보내면 됨
+    path('posts/<str:pk>/like', add_like),
+    path('posts/<str:pk>/dislike', add_dislike),
 ]
