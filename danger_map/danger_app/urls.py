888e54efa598b108624ebb40f3c6d52ebec1e6b7
jaychung0101 <jihoon20130@gmail.com>
2024-02-19 01:58:20 +0900

    Add marker

--- danger_map/danger_app/urls.py
+++ danger_map/danger_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_user_list, get_user, get_post_list, get_post, get_post_by_email, add_like, user_profile_pic, add_dislike, add_comment
+from .views import get_user_list, get_user, get_post_list, get_post_by_coord, get_marker, get_post, get_post_by_email, add_like, user_profile_pic, add_dislike, add_comment
 
 urlpatterns = [
     # user 컬렉션 기반
@@ -11,6 +11,10 @@ urlpatterns = [
     path('posts', get_post_list),
     path('posts/<str:type>', get_post), # danger_type을 type으로 받음
     path('posts/email/', get_post_by_email),
+
+    # 지도
+    path('markers', get_marker),
+    path('posts/location/', get_post_by_coord),
 
     # 좋아요, 싫어요 조회 및 추가
     path('posts/like/', add_like),
